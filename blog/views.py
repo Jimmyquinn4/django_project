b835650cc2b76c78c7e885ec5492e75db6c144e6
Jimmy Quinn <jimmyquinn4@gmail.com>
2019-02-18 15:05:36 -0500

    updated registrationg

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jimmy',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'February 11, 2019'
-
-    },
-    {
-        'author': 'NotJimmy',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 12, 2019'
-    }
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
